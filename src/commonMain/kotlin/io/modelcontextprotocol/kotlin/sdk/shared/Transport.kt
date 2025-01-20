cc501db1bfb8f88c85bd5b5cb157b75029f99931
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-01-20 19:12:48 +0100

    Fix AbstractTransport to not skip messages

--- src/commonMain/kotlin/io/modelcontextprotocol/kotlin/sdk/shared/Transport.kt
+++ src/commonMain/kotlin/io/modelcontextprotocol/kotlin/sdk/shared/Transport.kt
@@ -1,6 +1,7 @@
 package io.modelcontextprotocol.kotlin.sdk.shared
 
 import io.modelcontextprotocol.kotlin.sdk.JSONRPCMessage
+import kotlinx.coroutines.CompletableDeferred
 
 /**
  * Describes the minimal contract for a MCP transport that a client or server can communicate over.
@@ -54,8 +55,17 @@ public interface Transport {
 @Suppress("PropertyName")
 public abstract class AbstractTransport : Transport {
     protected var _onClose: (() -> Unit) = {}
+        private set
     protected var _onError: ((Throwable) -> Unit) = {}
-    protected var _onMessage: (suspend ((JSONRPCMessage) -> Unit)) = {}
+        private set
+
+    // to not skip messages
+    private val _onMessageInitialized = CompletableDeferred<Unit>()
+    protected var _onMessage: (suspend ((JSONRPCMessage) -> Unit)) = {
+        _onMessageInitialized.await()
+        _onMessage.invoke(it)
+    }
+        private set
 
     override fun onClose(block: () -> Unit) {
         val old = _onClose
@@ -74,10 +84,16 @@ public abstract class AbstractTransport : Transport {
     }
 
     override fun onMessage(block: suspend (JSONRPCMessage) -> Unit) {
-        val old = _onMessage
+        val old: suspend (JSONRPCMessage) -> Unit = when (_onMessageInitialized.isCompleted) {
+            true -> _onMessage
+            false -> { _ -> }
+        }
+
         _onMessage = { message ->
             old(message)
             block(message)
         }
+
+        _onMessageInitialized.complete(Unit)
     }
 }
