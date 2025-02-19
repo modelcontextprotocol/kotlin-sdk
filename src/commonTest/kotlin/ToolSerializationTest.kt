65089dc70ed4bde74ea65839a248ccca6075d6e0
Kazik Pogoda <morisil@xemantic.com>
2025-02-19 18:26:56 +0100

    ToolSerializationTest update

--- src/commonTest/kotlin/ToolSerializationTest.kt
+++ src/commonTest/kotlin/ToolSerializationTest.kt
@@ -6,42 +6,52 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.buildJsonObject
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ToolSerializationTest {
 
-    @Test
-    fun `should serialize GetWeather tool`() {
-        val tool = Tool(
-            name = "get_weather",
-            description = "Get the current weather in a given location",
-            inputSchema = Tool.Input(
-                properties = buildJsonObject {
-                    put("location", buildJsonObject {
-                        put("type", JsonPrimitive("string"))
-                        put("description", JsonPrimitive("The city and state, e.g. San Francisco, CA"))
-                    })
-                },
-                required = listOf("location")
-            )
-        )
-
-        // see https://docs.anthropic.com/en/docs/build-with-claude/tool-use
-        McpJson.encodeToString(tool) shouldEqualJson /* language=json */ """
-            {
-              "name": "get_weather",
-              "description": "Get the current weather in a given location",
-              "inputSchema": {
-                "type": "object",
-                "properties": {
-                  "location": {
-                    "type": "string",
-                    "description": "The city and state, e.g. San Francisco, CA"
-                  }
-                },
-                "required": ["location"]
+    // see https://docs.anthropic.com/en/docs/build-with-claude/tool-use
+    /* language=json */
+    private val getWeatherToolJson = """
+        {
+          "name": "get_weather",
+          "description": "Get the current weather in a given location",
+          "inputSchema": {
+            "type": "object",
+            "properties": {
+              "location": {
+                "type": "string",
+                "description": "The city and state, e.g. San Francisco, CA"
               }
-            }
-        """
+            },
+            "required": ["location"]
+          }
+        }
+    """.trimIndent()
+
+    val getWeatherTool = Tool(
+        name = "get_weather",
+        description = "Get the current weather in a given location",
+        inputSchema = Tool.Input(
+            properties = buildJsonObject {
+                put("location", buildJsonObject {
+                    put("type", JsonPrimitive("string"))
+                    put("description", JsonPrimitive("The city and state, e.g. San Francisco, CA"))
+                })
+            },
+            required = listOf("location")
+        )
+    )
+
+    @Test
+    fun `should serialize get_weather tool`() {
+        McpJson.encodeToString(getWeatherTool) shouldEqualJson getWeatherToolJson
+    }
+
+    @Test
+    fun `should deserialize get_weather tool`() {
+        val tool = McpJson.decodeFromString<Tool>(getWeatherToolJson)
+        assertEquals(expected = getWeatherTool, actual = tool)
     }
 
 }
