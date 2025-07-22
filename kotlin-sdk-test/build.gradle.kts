dfcc2426e29cd607893baf34ae66632cd2e38210
Sergey Karpov <sergey.karpov@jetbrains.com>
2025-07-22 17:42:11 +0300

    Introduce Kotlin integration tests
    Signed-off-by: Sergey Karpov <sergey.karpov@jetbrains.com>

--- kotlin-sdk-test/build.gradle.kts
+++ kotlin-sdk-test/build.gradle.kts
@@ -3,6 +3,11 @@ plugins {
 }
 
 kotlin {
+    jvm {
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
     sourceSets {
         commonTest {
             dependencies {
@@ -10,6 +15,14 @@ kotlin {
                 implementation(kotlin("test"))
                 implementation(libs.ktor.server.test.host)
                 implementation(libs.kotlinx.coroutines.test)
+            }
+        }
+        jvmTest {
+            dependencies {
+                implementation(kotlin("test-junit5"))
+                implementation(libs.kotlin.logging)
+                implementation(libs.ktor.server.cio)
+                implementation(libs.ktor.client.cio)
             }
         }
     }
