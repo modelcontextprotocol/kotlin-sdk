e749aaa06560f02915161fda4ebadf6d1cad9b96
Konstantin Pavlov <1517853+kpavlov@users.noreply.github.com>
2025-09-11 08:53:38 +0300

    Add Kover(age) check
    - Integrate Kover plugin for code coverage analysis
    - Run Kover tasks on CI
    - Publish Html report
    - Update actions/checkout@v5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.ktlint)
+    alias(libs.plugins.kover)
 }
 
 allprojects {
@@ -7,6 +8,31 @@ allprojects {
     version = "0.6.0"
 }
 
+dependencies {
+    kover(project(":kotlin-sdk-core"))
+    kover(project(":kotlin-sdk-client"))
+    kover(project(":kotlin-sdk-server"))
+    kover(project(":kotlin-sdk-test"))
+}
+
 subprojects {
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "org.jetbrains.kotlinx.kover")
+}
+
+kover {
+    reports {
+        filters {
+            includes.classes("io.modelcontextprotocol.kotlin.sdk.*")
+        }
+        total {
+            log {
+            }
+            verify {
+                rule {
+                    minBound(60)
+                }
+            }
+        }
+    }
 }
