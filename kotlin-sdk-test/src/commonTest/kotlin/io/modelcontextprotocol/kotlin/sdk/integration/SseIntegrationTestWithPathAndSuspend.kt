90f2e82f5e57b30e7b94ffdc8546c1acabd602e1
Iain Hull <iain.hull@workday.com>
2025-10-13 15:13:58 +0100

    Fixed imports with ktlint

--- kotlin-sdk-test/src/commonTest/kotlin/io/modelcontextprotocol/kotlin/sdk/integration/SseIntegrationTestWithPathAndSuspend.kt
+++ kotlin-sdk-test/src/commonTest/kotlin/io/modelcontextprotocol/kotlin/sdk/integration/SseIntegrationTestWithPathAndSuspend.kt
@@ -2,26 +2,23 @@ package io.modelcontextprotocol.kotlin.sdk.integration
 
 import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
-import io.ktor.server.routing.routing
 import io.ktor.server.routing.route
-import io.modelcontextprotocol.kotlin.sdk.server.mcp
+import io.ktor.server.routing.routing
 import io.modelcontextprotocol.kotlin.sdk.server.Server
-import kotlin.collections.emptyList
+import io.modelcontextprotocol.kotlin.sdk.server.mcp
 import io.ktor.server.cio.CIO as ServerCIO
 import io.ktor.server.sse.SSE as ServerSSE
 
 class SseIntegrationTestWithPathAndSuspend : AbstractSseIntegrationTest() {
 
-    private suspend fun suspendNewMcpServer(): Server {
-        return newMcpServer()
-    }
+    private suspend fun suspendNewMcpServer(): Server = newMcpServer()
 
     protected override suspend fun initServer(): Pair<CIOEmbeddedServer, List<String>> {
         val ktorServer = embeddedServer(ServerCIO, host = URL, port = PORT) {
             install(ServerSSE)
             routing {
                 route("/some-path") {
-                    mcp { 
+                    mcp {
                         suspendNewMcpServer()
                     }
                 }
@@ -31,4 +28,3 @@ class SseIntegrationTestWithPathAndSuspend : AbstractSseIntegrationTest() {
         return ktorServer.startSuspend(wait = false) to listOf("some-path")
     }
 }
-
