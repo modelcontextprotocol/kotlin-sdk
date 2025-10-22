f488364527bc761fbb86529e5481d64d9144799f
Konstantin Pavlov <1517853+kpavlov@users.noreply.github.com>
2025-10-22 15:53:13 +0300

    Add gradle to kotlin-mcp-client sample

--- samples/kotlin-mcp-client/settings.gradle.kts
+++ samples/kotlin-mcp-client/settings.gradle.kts
@@ -1,20 +1,13 @@
 rootProject.name = "kotlin-mcp-client"
 
-pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-    }
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 dependencyResolutionManagement {
     repositories {
         mavenLocal()
         mavenCentral()
-    }
-    versionCatalogs {
-        create("libs") {
-            from(files("../../gradle/libs.versions.toml"))
-        }
     }
 }
