ec81586668149cfb04bc3031aa841391f6fd63a5
devcrocod <devcrocod@gmail.com>
2026-01-30 15:45:13 +0100

    Remove tests with old schema

--- integration-test/src/jvmTest/kotlin/io/modelcontextprotocol/kotlin/sdk/integration/kotlin/stdio/OldSchemaResourceIntegrationTestStdio.kt
+++
@@ -1,7 +0,0 @@
-package io.modelcontextprotocol.kotlin.sdk.integration.kotlin.stdio
-
-import io.modelcontextprotocol.kotlin.sdk.integration.kotlin.OldSchemaAbstractResourceIntegrationTest
-
-class OldSchemaResourceIntegrationTestStdio : OldSchemaAbstractResourceIntegrationTest() {
-    override val transportKind: TransportKind = TransportKind.STDIO
-}
