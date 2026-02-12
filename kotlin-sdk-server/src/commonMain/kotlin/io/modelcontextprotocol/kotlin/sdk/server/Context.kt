f0a662b32e2cf4a5ac12f601589ab563435128a4
Ryan Nett <JNett96@gmail.com>
2026-02-11 17:37:52 -0800

    Annotate Context with ExperimentalMcpApi for inheritance

--- kotlin-sdk-server/src/commonMain/kotlin/io/modelcontextprotocol/kotlin/sdk/server/Context.kt
+++ kotlin-sdk-server/src/commonMain/kotlin/io/modelcontextprotocol/kotlin/sdk/server/Context.kt
@@ -1,10 +1,14 @@
 package io.modelcontextprotocol.kotlin.sdk.server
 
+import io.modelcontextprotocol.kotlin.sdk.ExperimentalMcpApi
+
 /**
  * Contextual information made available to server request handlers.
  */
+@SubclassOptInRequired(ExperimentalMcpApi::class)
 public interface Context {
     public val session: ServerSession
 }
 
+@OptIn(ExperimentalMcpApi::class)
 internal class ContextImpl(override val session: ServerSession) : Context
