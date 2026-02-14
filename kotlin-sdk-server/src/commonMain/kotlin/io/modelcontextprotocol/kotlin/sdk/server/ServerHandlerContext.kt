032bfa2dcda96d783a8a84ca492abb3ee4fb0822
Ryan Nett <JNett96@gmail.com>
2026-02-14 14:22:40 -0800

    Allow calling the session communication messages directly on the context

--- kotlin-sdk-server/src/commonMain/kotlin/io/modelcontextprotocol/kotlin/sdk/server/ServerHandlerContext.kt
+++ kotlin-sdk-server/src/commonMain/kotlin/io/modelcontextprotocol/kotlin/sdk/server/ServerHandlerContext.kt
@@ -6,9 +6,11 @@ import io.modelcontextprotocol.kotlin.sdk.ExperimentalMcpApi
  * Contextual information made available to server request handlers.
  */
 @SubclassOptInRequired(ExperimentalMcpApi::class)
-public interface ServerHandlerContext {
+public interface ServerHandlerContext : ServerCommunication {
     public val session: ServerSession
 }
 
 @OptIn(ExperimentalMcpApi::class)
-internal class ServerHandlerContextImpl(override val session: ServerSession) : ServerHandlerContext
+internal class ServerHandlerContextImpl(override val session: ServerSession) :
+    ServerHandlerContext,
+    ServerCommunication by session
