3461ae90d54d3462b197a8e7106bd34033accbaf
Ryan Nett <JNett96@gmail.com>
2026-02-18 21:01:21 -0800

    Revert "Remove unnecessary suppression"
    This reverts commit 835d9f30efaf2e8bc1203430a34eef990ee5bb77.

--- kotlin-sdk-server/src/commonMain/kotlin/io/modelcontextprotocol/kotlin/sdk/server/ServerSession.kt
+++ kotlin-sdk-server/src/commonMain/kotlin/io/modelcontextprotocol/kotlin/sdk/server/ServerSession.kt
@@ -37,6 +37,7 @@ private val logger = KotlinLogging.logger {}
  * Represents a server session.
  */
 @OptIn(ExperimentalMcpApi::class)
+@Suppress("TooManyFunctions")
 public open class ServerSession(
     protected val serverInfo: Implementation,
     options: ServerOptions,
